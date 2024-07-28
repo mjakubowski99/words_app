0708e237e44194e0944fadf6006250f0ba061184
mjakubowski99 <michal.j@devpark.pl>
2024-07-28 20:01:03 +0200

    Add cors support

---
+++ entry/app/Http/Middleware/Cors.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Middleware;
+
+use Illuminate\Http\Request;
+
+class Cors
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param Request $request
+     *
+     * @return mixed
+     */
+    public function handle($request, \Closure $next)
+    {
+        $response = $next($request);
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+        $response->headers->set('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE');
+        $response->headers->set(
+            'Access-Control-Allow-Headers',
+            'Content-Type, Accept, Authorization, X-Requested-With, Application, WebbrowserTabId'
+        );
+
+        return $response;
+    }
+}
