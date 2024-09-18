438b86d6e10715636013b3490e9644ddeab65043
mjakubowski99 <michal.j@devpark.pl>
2024-09-18 12:20:08 +0200

    Fix migrations

--- entry/database/migrations/2024_08_29_165357_create_learning_sessions_table.php
+++ entry/database/migrations/2024_08_29_165357_create_learning_sessions_table.php
@@ -36,6 +36,6 @@ return new class() extends Migration {
      */
     public function down(): void
     {
-        Schema::dropIfExists('sessions');
+        Schema::dropIfExists('learning_sessions');
     }
 };
