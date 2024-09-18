31e24b234b6e32959d5b1ceda982dac49890f44e
mjakubowski99 <michal.j@devpark.pl>
2024-09-18 12:17:11 +0200

    Fix migrations

--- entry/database/migrations/2024_08_29_181823_create_learning_session_flashcards_table.php
+++ entry/database/migrations/2024_08_29_181823_create_learning_session_flashcards_table.php
@@ -35,6 +35,6 @@ return new class() extends Migration {
      */
     public function down(): void
     {
-        Schema::dropIfExists('session_flashcards');
+        Schema::dropIfExists('learning_session_flashcards');
     }
 };
