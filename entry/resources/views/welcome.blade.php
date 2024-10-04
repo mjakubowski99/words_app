5da485355384e53e605b1127e778226b44d46e9a
mjakubowski99 <michal.j@devpark.pl>
2024-10-05 01:09:11 +0200

    Remove files

--- entry/resources/views/welcome.blade.php
+++ entry/resources/views/welcome.blade.php
@@ -7,24 +7,7 @@
     <script src="https://www.gstatic.com/firebasejs/8.6.8/firebase-app.js"></script>
     <script src="https://www.gstatic.com/firebasejs/8.6.8/firebase-auth.js"></script>
     <script>
-        const firebaseConfig = {
-
-            apiKey: "AIzaSyDXViUhI-4C4jE3KeutGG4N7hKKmvi8q3c",
-
-            authDomain: "vocasmart-49389.firebaseapp.com",
-
-            projectId: "vocasmart-49389",
-
-            storageBucket: "vocasmart-49389.appspot.com",
-
-            messagingSenderId: "588544931926",
-
-            appId: "1:588544931926:web:cdcfdb4bab6c10a6dba3f7",
-
-            measurementId: "G-GE4HPMFLZW"
-
-        };
-
+        const firebaseConfig = {};
 
         firebase.initializeApp(firebaseConfig);
 
