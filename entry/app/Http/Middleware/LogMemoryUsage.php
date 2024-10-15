9567cae20f867ff710e0b9d01658ded240fee9f0
mjakubowski99 <michal.j@devpark.pl>
2024-10-15 09:24:26 +0200

    Update request logger

--- entry/app/Http/Middleware/LogMemoryUsage.php
+++ entry/app/Http/Middleware/LogMemoryUsage.php
@@ -18,17 +18,23 @@ class LogMemoryUsage
     {
         // Uruchomienie pomiaru pamięci przed przetworzeniem żądania
         $startMemory = memory_get_usage();
+        $peakStart = memory_get_peak_usage();
 
         // Przetworzenie żądania
         $response = $next($request);
 
         // Po przetworzeniu żądania, obliczamy zużycie pamięci
         $endMemory = memory_get_usage();
+        $peakEnd = memory_get_peak_usage();
         $memoryUsed = $endMemory - $startMemory;
 
         $memoryUsedKB = $memoryUsed / 1024; // W kilobajtach
 
         Log::info('Zużycie pamięci: ' . round($memoryUsedKB, 2) . ' KB');
+
+        Log::info('Zużycie pamięci peak start: ' . round($peakStart/1024, 2) . ' KB');
+        Log::info('Zużycie pamięci peak end: ' . round($peakEnd/1024, 2) . ' KB');
+
 
         return $response;
     }
