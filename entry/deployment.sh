378e626144e7aee54911ae6b795ddc9834aedcee
mjakubowski99 <michal.j@devpark.pl>
2024-10-30 12:05:08 +0100

    Add deploy.sh scripts

--- entry/deployment.sh
+++ entry/deployment.sh
@@ -1,12 +1,49 @@
 #!/bin/bash
 
-cd docker/ &&
-docker-compose -f docker-compose.prod.yml build &&
-docker-compose -f docker-compose.prod.yml down &&
-docker-compose -f docker-compose.prod.yml up -d &&
-docker exec words_prod_php php artisan optimize:clear &&
-docker exec words_prod_php php artisan optimize &&
-docker exec words_prod_php php artisan migrate --force
+RESET='\e[0m'
+GREEN='\e[32m'
+RED='\e[31m'
+
+set -o allexport
+source .env
+set +o allexport
+
+SERVER_USER=$(echo "$SSH_USER" | base64 --decode)
+SERVER_HOST=$(echo "$SSH_HOST" | base64 --decode)
+SERVER_PORT=$(echo "$SSH_PORT" | base64 --decode)
+SERVER_PASS=$(echo "$SSH_PASSWORD" | base64 --decode)
+
+if [[ -z "$SERVER_USER" || -z "$SERVER_HOST" || -z "$SERVER_PORT" || -z "$SERVER_PASS" ]]; then
+  echo "Error: SERVER_USER, SERVER_HOST, SERVER_PORT, and SERVER_PASS must be set and base64 encoded in the .env file."
+  exit 1
+fi
+
+if ! command -v sshpass &> /dev/null; then
+  echo "Error: sshpass is not installed. Please install it and try again."
+  exit 1
+fi
+
+# Definicja komend do uruchomienia na serwerze z użyciem heredoc
+REMOTE_COMMANDS=$(cat <<EOF
+set -e  # Przerwij wykonanie przy błędzie dowolnej komendy
+echo -e '${GREEN}Połączono pomyślnie!${RESET}'
+cd /home/$SERVER_USER/voca_app/entry/prod \
+
+{
+  docker compose build php \
+  && docker compose down php \
+  && docker compose up -d php \
+  && docker compose exec php php artisan optimize:clear \
+  && docker compose exec php php artisan optimize \
+  && docker compose exec php php artisan migrate --force \
+  && echo -e '${GREEN}Successfully deployed!${RESET}'
+} || {
+  echo -e "${RED}Deployment failed.${RESET}" 1>&2
+}
+EOF
+)
+
+sshpass -p "$SERVER_PASS" ssh -p "$SERVER_PORT" "$SERVER_USER@$SERVER_HOST" "$REMOTE_COMMANDS"
 
 
 
