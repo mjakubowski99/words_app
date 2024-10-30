9f70c7737f57a86c8cd3fd22150940ed42061ed8
mjakubowski99 <michal.j@devpark.pl>
2024-10-30 11:25:45 +0100

    Small refactor

--- User/Infrastructure/OAuth/Google/IosGoogleClient.php
+++ User/Infrastructure/OAuth/Google/IosGoogleClient.php
@@ -4,12 +4,15 @@ declare(strict_types=1);
 
 namespace User\Infrastructure\OAuth\Google;
 
+use Shared\Enum\UserProvider;
+
 class IosGoogleClient extends \Google_Client
 {
     public function __construct()
     {
+        $provider = UserProvider::GOOGLE;
         parent::__construct([
-            'client_id' => config('services.google.ios_client_id'),
+            'client_id' => config("services.{$provider->value}.ios_client_id"),
         ]);
     }
 }
