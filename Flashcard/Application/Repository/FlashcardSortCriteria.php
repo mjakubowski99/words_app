c05266e93fa10a80fc2fd2c371cca7dabc6e1879
mjakubowski99 <michal.j@devpark.pl>
2024-11-24 20:47:25 +0100

    Flashcard sort criteria

--- Flashcard/Application/Repository/FlashcardSortCriteria.php
+++ Flashcard/Application/Repository/FlashcardSortCriteria.php
@@ -6,11 +6,11 @@ namespace Flashcard\Application\Repository;
 
 enum FlashcardSortCriteria: string
 {
-    case NOT_RATED_FLASHCARDS_FIRST = 'not_rated_flashcards_first';
-    case HARD_FLASHCARDS_FIRST = 'hard_flashcards_first';
-    case LOWEST_REPETITION_INTERVAL_FIRST = 'by_repetition_interval';
-    case PLANNED_FLASHCARDS_FOR_CURRENT_DATE_FIRST = 'by_planned_flashcards_for_current_date';
-    case OLDEST_UPDATE_FLASHCARDS_FIRST = 'by_oldest_update';
-    case NOT_HARD_FLASHCARDS_FIRST = 'repetition_interval_greater_than_one_first';
-    case RANDOMIZE_LATEST_FLASHCARDS_ORDER = 'randomize_latest_flashcards_order';
+    case NOT_RATED_FLASHCARDS_FIRST = 'NOT_RATED_FLASHCARDS_FIRST';
+    case HARD_FLASHCARDS_FIRST = 'HARD_FLASHCARDS_FIRST';
+    case LOWEST_REPETITION_INTERVAL_FIRST = 'LOWEST_REPETITION_INTERVAL_FIRST';
+    case PLANNED_FLASHCARDS_FOR_CURRENT_DATE_FIRST = 'PLANNED_FLASHCARDS_FOR_CURRENT_DATE_FIRST';
+    case OLDEST_UPDATE_FLASHCARDS_FIRST = 'OLDEST_UPDATE_FLASHCARDS_FIRST';
+    case NOT_HARD_FLASHCARDS_FIRST = 'NOT_HARD_FLASHCARDS_FIRST';
+    case RANDOMIZE_LATEST_FLASHCARDS_ORDER = 'RANDOMIZE_LATEST_FLASHCARDS_ORDER';
 }
