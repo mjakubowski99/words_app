d154fddae6fa64af80babcb9964f393259558460
mjakubowski99 <michal.j@devpark.pl>
2025-01-03 16:28:11 +0100

    Add description of import flashcard action

--- Admin/Traits/HasFlashcardDeckConfigurator.php
+++ Admin/Traits/HasFlashcardDeckConfigurator.php
@@ -38,7 +38,19 @@ trait HasFlashcardDeckConfigurator
             ->form([
                 TextInput::make('name')->required(),
                 Select::make('language_level')->options(LanguageLevel::getForSelectOptions())->required(),
-                FileUpload::make('import_file')->required(),
+                FileUpload::make('import_file')
+                    ->required()
+                    ->acceptedFileTypes([
+                        'text/csv'
+                    ])
+                    ->helperText(
+                        "File should be CSV file with given structure: " .
+                        "front_word(word on front of the card), " .
+                        "front_context(context sentence on the end of the card), " .
+                        "back_word(word on the back of the card), " .
+                        "back_context(context sentence on the back of the card). " .
+                        "If those headers are missing import will fail\n"
+                    )
             ])->action(function (array $data) {
                 $file = Storage::disk('public')->path($data['import_file']);
 
