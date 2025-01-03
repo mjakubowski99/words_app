37076faaab11494c47055204962a3c741a20cdd1
mjakubowski99 <michal.j@devpark.pl>
2025-01-03 16:28:49 +0100

    Cs fixer applied

--- Admin/Traits/HasFlashcardDeckConfigurator.php
+++ Admin/Traits/HasFlashcardDeckConfigurator.php
@@ -41,16 +41,16 @@ trait HasFlashcardDeckConfigurator
                 FileUpload::make('import_file')
                     ->required()
                     ->acceptedFileTypes([
-                        'text/csv'
+                        'text/csv',
                     ])
                     ->helperText(
-                        "File should be CSV file with given structure: " .
-                        "front_word(word on front of the card), " .
-                        "front_context(context sentence on the end of the card), " .
-                        "back_word(word on the back of the card), " .
-                        "back_context(context sentence on the back of the card). " .
+                        'File should be CSV file with given structure: ' .
+                        'front_word(word on front of the card), ' .
+                        'front_context(context sentence on the end of the card), ' .
+                        'back_word(word on the back of the card), ' .
+                        'back_context(context sentence on the back of the card). ' .
                         "If those headers are missing import will fail\n"
-                    )
+                    ),
             ])->action(function (array $data) {
                 $file = Storage::disk('public')->path($data['import_file']);
 
